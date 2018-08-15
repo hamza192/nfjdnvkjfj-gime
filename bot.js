d6242daa9dc4b7cd30946940d5c5d0fab881d1d2
hamza192 <41551916+hamza192@users.noreply.github.com>
2018-08-15 23:36:17 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,7 +26,7 @@ client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
   console.log('╚[════════════]╝')
   console.log('')
   console.log('')
-});
+
 
 let points = JSON.parse(fs.readFileSync('points.json', 'utf8'));
 client.on('message', message => {
