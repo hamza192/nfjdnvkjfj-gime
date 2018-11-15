acb3d9ef8e900e415a86cf42133b1c0effe731ae
hamza192 <41551916+hamza192@users.noreply.github.com>
2018-11-15 21:05:09 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -52,60 +52,6 @@ var q3 = ".quran 3"
 
 var q4 = ".quran 4"
 
-var q5 = ".quran 5"
-
-var q6 = ".quran 6"
-
-var q7 = ".quran 7"
-
-var q8 = ".quran 8"
-
-var q9 = ".quran 9"
-
-var q10 = ".quran 10"
-
-var q11 = ".quran 11"
-
-var q12 = ".quran 12"
-
-var q13 = ".quran 13"
-
-var q14 = ".quran 14"
-
-var q15 = ".quran 15"
-
-var q16 = ".quran 16"
-
-var q17 = ".quran 17"
-
-var q18 = ".quran 18"
-
-var q19 = ".quran 19"
-
-var q20 = ".quran 20"
-
-var q21 = ".quran 21"
-
-var q22 = ".quran 22"
-
-var q23 = ".quran 23"
-
-var q24 = ".quran 24"
-
-var q25 = ".quran 25"
-
-var q26 = ".quran 26"
-
-var q27 = ".quran 27"
-
-var q28 = ".quran 28"
-
-var q29 = ".quran 29"
-
-var q30 = ".quran 30"
-
-var q31 = ".quran 31"
-
 
 
 
@@ -134,8 +80,10 @@ var PREFIX = ".";
 //sowar
 
 
-if (message.content === q1 ) {
-                  message.react(':loud_sound:')
+client.on("message", message => {
+
+	                    if (message.content === q1 ) {
+                  message.react('🔊')
     const voiceChannel = message.member.voiceChannel;
     if (!voiceChannel) {
       return message.reply(`يرجى أن تكون في قناة صوتيه أولا!`);
@@ -148,516 +96,43 @@ if (message.content === q1 ) {
   }
   
   	                    if (message.content === q2 ) {
-                  message.react(':loud_sound:')
+                  message.react('🔊')
     const voiceChannel = message.member.voiceChannel;
     if (!voiceChannel) {
       return message.reply(`يرجى أن تكون في قناة صوتيه أولا!`);
     }
     voiceChannel.join()
       .then(connnection => {
-        let stream = yt('https://www.youtube.com/watch?v=2ZuDh0vqxUE', {audioonly: true});
+        let stream = yt('https://youtu.be/0m02xNtR8gA', {audioonly: true});
         const dispatcher = connnection.playStream(stream);
       });
   }
   
     	                    if (message.content === q3 ) {
-                  message.react(':loud_sound:')
+                  message.react('🔊')
     const voiceChannel = message.member.voiceChannel;
     if (!voiceChannel) {
       return message.reply(`يرجى أن تكون في قناة صوتيه أولا!`);
     }
     voiceChannel.join()
       .then(connnection => {
-        let stream = yt('https://www.youtube.com/watch?v=gIYaTs1Kw90', {audioonly: true});
+        let stream = yt('https://www.youtube.com/watch?v=4JvY-MccxNk', {audioonly: true});
         const dispatcher = connnection.playStream(stream);
       });
   }
   
-      	              
-
-
-
-
-
-
-
-
-
-      if (message.content === q4 ) {
-                  message.react(':loud_sound:')
+      	                    if (message.content === q4 ) {
+                  message.react('🔊')
     const voiceChannel = message.member.voiceChannel;
     if (!voiceChannel) {
       return message.reply(`يرجى أن تكون في قناة صوتيه أولا!`);
     }
     voiceChannel.join()
       .then(connnection => {
-        let stream = yt('https://www.youtube.com/watch?v=F9w_BGTVY4o', {audioonly: true});
+        let stream = yt('https://www.youtube.com/watch?v=Ktync4j_nmA', {audioonly: true});
         const dispatcher = connnection.playStream(stream);
         });
   }
- 
-    
-	
-	if (message.content === q5 ) {
-                  message.react(':loud_sound:')
-    const voiceChannel = message.member.voiceChannel;
-    if (!voiceChannel) {
-      return message.reply(`يرجى أن تكون في قناة صوتيه أولا!`);
-    }
-    voiceChannel.join()
-      .then(connnection => {
-        let stream = yt('https://www.youtube.com/watch?v=sOML64y5dfQ', {audioonly: true});
-        const dispatcher = connnection.playStream(stream);
-        });
-  }
-	
-	
-	
-	
-	
-	if (message.content === q6 ) {
-                  message.react(':loud_sound:')
-    const voiceChannel = message.member.voiceChannel;
-    if (!voiceChannel) {
-      return message.reply(`يرجى أن تكون في قناة صوتيه أولا!`);
-    }
-    voiceChannel.join()
-      .then(connnection => {
-        let stream = yt('https://www.youtube.com/watch?v=Pbbl-iZbhb4', {audioonly: true});
-        const dispatcher = connnection.playStream(stream);
-        });
-  }
-    
-	
-	if (message.content === q7 ) {
-                  message.react(':loud_sound:')
-    const voiceChannel = message.member.voiceChannel;
-    if (!voiceChannel) {
-      return message.reply(`يرجى أن تكون في قناة صوتيه أولا!`);
-    }
-    voiceChannel.join()
-      .then(connnection => {
-        let stream = yt('https://www.youtube.com/watch?v=Y5YKlCIn9aE', {audioonly: true});
-        const dispatcher = connnection.playStream(stream);
-        });
-  }
-	
-	
-	
-	
-if (message.content === q8 ) {
-                  message.react(':loud_sound:')
-    const voiceChannel = message.member.voiceChannel;
-    if (!voiceChannel) {
-      return message.reply(`يرجى أن تكون في قناة صوتيه أولا!`);
-    }
-    voiceChannel.join()
-      .then(connnection => {
-        let stream = yt('https://www.youtube.com/watch?v=0rY0n4xEs44', {audioonly: true});
-        const dispatcher = connnection.playStream(stream);
-        });
-  }	
-	
-	
-	
-	
-	if (message.content === q9 ) {
-                  message.react(':loud_sound:')
-    const voiceChannel = message.member.voiceChannel;
-    if (!voiceChannel) {
-      return message.reply(`يرجى أن تكون في قناة صوتيه أولا!`);
-    }
-    voiceChannel.join()
-      .then(connnection => {
-        let stream = yt('https://www.youtube.com/watch?v=vjGEP4rW_l4', {audioonly: true});
-        const dispatcher = connnection.playStream(stream);
-        });
-  }
-	
-	
-	
-	
-	
-	
-	
-	if (message.content === q10 ) {
-                  message.react(':loud_sound:')
-    const voiceChannel = message.member.voiceChannel;
-    if (!voiceChannel) {
-      return message.reply(`يرجى أن تكون في قناة صوتيه أولا!`);
-    }
-    voiceChannel.join()
-      .then(connnection => {
-        let stream = yt('https://www.youtube.com/watch?v=7vrX1vIyRzw', {audioonly: true});
-        const dispatcher = connnection.playStream(stream);
-        });
-  }
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	if (message.content === q11 ) {
-                  message.react(':loud_sound:')
-    const voiceChannel = message.member.voiceChannel;
-    if (!voiceChannel) {
-      return message.reply(`يرجى أن تكون في قناة صوتيه أولا!`);
-    }
-    voiceChannel.join()
-      .then(connnection => {
-        let stream = yt('https://www.youtube.com/watch?v=tuYP5JktEn0', {audioonly: true});
-        const dispatcher = connnection.playStream(stream);
-        });
-  }
-	
-	
-	
-	
-	if (message.content === q12 ) {
-                  message.react(':loud_sound:')
-    const voiceChannel = message.member.voiceChannel;
-    if (!voiceChannel) {
-      return message.reply(`يرجى أن تكون في قناة صوتيه أولا!`);
-    }
-    voiceChannel.join()
-      .then(connnection => {
-        let stream = yt('https://www.youtube.com/watch?v=4fugPfoybFI', {audioonly: true});
-        const dispatcher = connnection.playStream(stream);
-        });
-  }
-	
-	
-	
-	if (message.content === q13 ) {
-                  message.react(':loud_sound:')
-    const voiceChannel = message.member.voiceChannel;
-    if (!voiceChannel) {
-      return message.reply(`يرجى أن تكون في قناة صوتيه أولا!`);
-    }
-    voiceChannel.join()
-      .then(connnection => {
-        let stream = yt('https://www.youtube.com/watch?v=YuiDHf624tY', {audioonly: true});
-        const dispatcher = connnection.playStream(stream);
-        });
-  }
-	
-	
-	
-	
-	if (message.content === q14) {
-                  message.react(':loud_sound:')
-    const voiceChannel = message.member.voiceChannel;
-    if (!voiceChannel) {
-      return message.reply(`يرجى أن تكون في قناة صوتيه أولا!`);
-    }
-    voiceChannel.join()
-      .then(connnection => {
-        let stream = yt('https://www.youtube.com/watch?v=YrWbQz7M2EA', {audioonly: true});
-        const dispatcher = connnection.playStream(stream);
-        });
-  }
-	
-	
-	
-	
-	if (message.content === q15) {
-                  message.react(':loud_sound:')
-    const voiceChannel = message.member.voiceChannel;
-    if (!voiceChannel) {
-      return message.reply(`يرجى أن تكون في قناة صوتيه أولا!`);
-    }
-    voiceChannel.join()
-      .then(connnection => {
-        let stream = yt('https://www.youtube.com/watch?v=siH3Rm3pqkw', {audioonly: true});
-        const dispatcher = connnection.playStream(stream);
-        });
-  }
-	
-	
-	
-	
-	
-	
-	if (message.content === q16) {
-                  message.react(':loud_sound:')
-    const voiceChannel = message.member.voiceChannel;
-    if (!voiceChannel) {
-      return message.reply(`يرجى أن تكون في قناة صوتيه أولا!`);
-    }
-    voiceChannel.join()
-      .then(connnection => {
-        let stream = yt('https://www.youtube.com/watch?v=vD4fLi_MtKs', {audioonly: true});
-        const dispatcher = connnection.playStream(stream);
-        });
-  }
-	
-
-
-
-
-cif (message.content === q17) {
-                  message.react(':loud_sound:')
-    const voiceChannel = message.member.voiceChannel;
-    if (!voiceChannel) {
-      return message.reply(`يرجى أن تكون في قناة صوتيه أولا!`);
-    }
-    voiceChannel.join()
-      .then(connnection => {
-        let stream = yt('https://www.youtube.com/watch?v=5jovCoS5Fbw', {audioonly: true});
-        const dispatcher = connnection.playStream(stream);
-        });
-  }
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	if (message.content === q18) {
-                  message.react(':loud_sound:')
-    const voiceChannel = message.member.voiceChannel;
-    if (!voiceChannel) {
-      return message.reply(`يرجى أن تكون في قناة صوتيه أولا!`);
-    }
-    voiceChannel.join()
-      .then(connnection => {
-        let stream = yt('https://www.youtube.com/watch?v=bNYNoM-d444', {audioonly: true});
-        const dispatcher = connnection.playStream(stream);
-        });
-  }
-	
-	
-	
-	
-	
-	
-	if (message.content === q19) {
-                  message.react(':loud_sound:')
-    const voiceChannel = message.member.voiceChannel;
-    if (!voiceChannel) {
-      return message.reply(`يرجى أن تكون في قناة صوتيه أولا!`);
-    }
-    voiceChannel.join()
-      .then(connnection => {
-        let stream = yt('https://www.youtube.com/watch?v=jIhMpnx5ceE', {audioonly: true});
-        const dispatcher = connnection.playStream(stream);
-        });
-  }
-	
-	
-	
-	
-	
-	if (message.content === q20) {
-                  message.react(':loud_sound:')
-    const voiceChannel = message.member.voiceChannel;
-    if (!voiceChannel) {
-      return message.reply(`يرجى أن تكون في قناة صوتيه أولا!`);
-    }
-    voiceChannel.join()
-      .then(connnection => {
-        let stream = yt('https://www.youtube.com/watch?v=cJNKDUqkbkc', {audioonly: true});
-        const dispatcher = connnection.playStream(stream);
-        });
-  }
-	
-	
-	
-	if (message.content === q21) {
-                  message.react(':loud_sound:')
-    const voiceChannel = message.member.voiceChannel;
-    if (!voiceChannel) {
-      return message.reply(`يرجى أن تكون في قناة صوتيه أولا!`);
-    }
-    voiceChannel.join()
-      .then(connnection => {
-        let stream = yt('https://www.youtube.com/watch?v=00A-STn5M0E', {audioonly: true});
-        const dispatcher = connnection.playStream(stream);
-        });
-  }
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	if (message.content === q22) {
-                  message.react(':loud_sound:')
-    const voiceChannel = message.member.voiceChannel;
-    if (!voiceChannel) {
-      return message.reply(`يرجى أن تكون في قناة صوتيه أولا!`);
-    }
-    voiceChannel.join()
-      .then(connnection => {
-        let stream = yt('https://www.youtube.com/watch?v=00A-STn5M0E', {audioonly: true});
-        const dispatcher = connnection.playStream(stream);
-        });
-  }
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	g
-	g
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	if (message.content === q25) {
-                  message.react(':loud_sound:')
-    const voiceChannel = message.member.voiceChannel;
-    if (!voiceChannel) {
-      return message.reply(`يرجى أن تكون في قناة صوتيه أولا!`);
-    }
-    voiceChannel.join()
-      .then(connnection => {
-        let stream = yt('https://www.youtube.com/watch?v=Cg1TjxQeV2M', {audioonly: true});
-        const dispatcher = connnection.playStream(stream);
-        });
-  }
-	
-	
-	
-	
-	
-	
-	if (message.content === q26) {
-                  message.react(':loud_sound:')
-    const voiceChannel = message.member.voiceChannel;
-    if (!voiceChannel) {
-      return message.reply(`يرجى أن تكون في قناة صوتيه أولا!`);
-    }
-    voiceChannel.join()
-      .then(connnection => {
-        let stream = yt('https://www.youtube.com/watch?v=vSOl00Y-WDo', {audioonly: true});
-        const dispatcher = connnection.playStream(stream);
-        });
-  }
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	if (message.content === q27 ) {
-                  message.react(':loud_sound:')
-    const voiceChannel = message.member.voiceChannel;
-    if (!voiceChannel) {
-      return message.reply(`يرجى أن تكون في قناة صوتيه أولا!`);
-    }
-    voiceChannel.join()
-      .then(connnection => {
-        let stream = yt('https://www.youtube.com/watch?v=QO2BN916CKI', {audioonly: true});
-        const dispatcher = connnection.playStream(stream);
-        });
-  }
-	
-	
-	
-	
-	
-	if (message.content === q28) {
-                  message.react(':loud_sound:')
-    const voiceChannel = message.member.voiceChannel;
-    if (!voiceChannel) {
-      return message.reply(`يرجى أن تكون في قناة صوتيه أولا!`);
-    }
-    voiceChannel.join()
-      .then(connnection => {
-        let stream = yt('https://www.youtube.com/watch?v=-q5P4hH46vU', {audioonly: true});
-        const dispatcher = connnection.playStream(stream);
-        });
-  }
-	
-	
-	
-	
-	
-	if (message.content === q29) {
-                  message.react(':loud_sound:')
-    const voiceChannel = message.member.voiceChannel;
-    if (!voiceChannel) {
-      return message.reply(`يرجى أن تكون في قناة صوتيه أولا!`);
-    }
-    voiceChannel.join()
-      .then(connnection => {
-        let stream = yt('https://www.youtube.com/watch?v=k-71LO_6jMM', {audioonly: true});
-        const dispatcher = connnection.playStream(stream);
-        });
-  }
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	if (message.content === q30) {
-                  message.react(':loud_sound:')
-    const voiceChannel = message.member.voiceChannel;
-    if (!voiceChannel) {
-      return message.reply(`يرجى أن تكون في قناة صوتيه أولا!`);
-    }
-    voiceChannel.join()
-      .then(connnection => {
-        let stream = yt('https://www.youtube.com/watch?v=hbELjHYr11Q', {audioonly: true});
-        const dispatcher = connnection.playStream(stream);
-        });
-  }
-	
-	
-
-
-	if (message.content === q31 {
-                  message.react(':loud_sound:')
-    const voiceChannel = message.member.voiceChannel;
-    if (!voiceChannel) {
-      return message.reply(`يرجى أن تكون في قناة صوتيه أولا!`);
-    }
-    voiceChannel.join()
-      .then(connnection => {
-        let stream = yt('https://www.youtube.com/watch?v=EjeOlh8DQZw', {audioonly: true});
-        const dispatcher = connnection.playStream(stream);
-        });
-  }
- 
  
     
     
@@ -686,7 +161,6 @@ cif (message.content === q17) {
  
 	    
 });
-
 
 
 
