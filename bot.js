954ad25f360a9e63084af2667584279486c007d7
hamza192 <41551916+hamza192@users.noreply.github.com>
2018-11-17 15:32:45 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -523,7 +523,8 @@ client.on("message", message => {
 	
 	
 	
-	if (message.content === q24
+	
+	if (message.content === q25) {
                   message.react('🔊')
     const voiceChannel = message.member.voiceChannel;
     if (!voiceChannel) {
@@ -531,13 +532,10 @@ client.on("message", message => {
     }
     voiceChannel.join()
       .then(connnection => {
-        let stream = yt('https://www.youtube.com/watch?v=7UaT5Q8IvVs', {audioonly: true});
+        let stream = yt('https://www.youtube.com/watch?v=7UaT5Q8IvVs, {audioonly: true});
         const dispatcher = connnection.playStream(stream);
         });
   }
-	
-	
-	
 	
 	
 	
